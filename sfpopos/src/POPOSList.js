f9efbeaabeaddcb1a27d03e7f8e757773e8d58e4
Salo <salomonco@gmail.com>
2022-10-22 13:54:30 -0500

    component built

---
+++ sfpopos/src/POPOSList.js
@@ -0,0 +1,37 @@
+// src/POPOSList.js
+
+import POPOSSpace from "./POPOSSpace";
+
+function POPOSList() {
+  return (
+    <div className="POPOSList">
+      <POPOSSpace
+        name="50 California Street"
+        address="50 California St."
+        image="50-california-st.jpg"
+      />
+      <POPOSSpace
+        name="100-pine"
+        address="100-pine"
+        image="100-pine.jpg"
+      />{" "}
+      <POPOSSpace
+        name="101"
+        address="101"
+        image="101-california.jpg"
+      />{" "}
+      <POPOSSpace
+        name="50 California Street"
+        address="50 California St."
+        image="50-california-st.jpg"
+      />{" "}
+      <POPOSSpace
+        name="50 California Street"
+        address="50 California St."
+        image="50-california-st.jpg"
+      />
+    </div>
+  );
+}
+
+export default POPOSList;
