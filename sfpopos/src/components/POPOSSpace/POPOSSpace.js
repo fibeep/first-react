8336e4f539381c9189a7c0dabec47988cb0fdeb4
Salo <salomonco@gmail.com>
2022-10-23 15:06:09 -0500

    upgraded styles for Spaces

--- sfpopos/src/components/POPOSSpace/POPOSSpace.js
+++ sfpopos/src/components/POPOSSpace/POPOSSpace.js
@@ -7,8 +7,8 @@ import { Link } from "react-router-dom";
 function POPOSSpace(props) {
   const { name, image, address, hours, id } = props;
   return (
-    <div>
-      <Link to={`/details/${id}`}>
+    <div className="POPOSSpace">
+      <Link className="POPOSSpace-title" to={`/details/${id}`}>
         <img
           src={`${process.env.PUBLIC_URL}/images/${image}`}
           width="300"
@@ -20,8 +20,10 @@ function POPOSSpace(props) {
       <h1>
         <Link to={`/details/${id}`}>{name}</Link>
       </h1>
-      <div>{address}</div>
-      <div>{hours}</div>
+      <div className="POPOSSpace-info">
+        <div>{address}</div>
+        <div>{hours}</div>
+      </div>
     </div>
   );
 }
