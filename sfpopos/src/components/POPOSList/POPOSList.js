6a47bbb5f2ab1d7dca7285aa7dbda9bfff1e5e3c
Salo <salomonco@gmail.com>
2022-10-22 20:43:43 -0500

    impropved css

--- sfpopos/src/components/POPOSList/POPOSList.js
+++ sfpopos/src/components/POPOSList/POPOSList.js
@@ -6,15 +6,17 @@ import "./POPOSList.css";
 function POPOSList() {
 const spaces = data.map(( { title, address, images, hours }, i) => {
   return (
-    <POPOSSpace
-      id = {i}
-      key={title}
-      name={title}
-      address={address}
-      image={images[0]}
-      hours={hours}
-    />
-  )
+    <div className="POPOSLIST">
+      <POPOSSpace
+        id={i}
+        key={title}
+        name={title}
+        address={address}
+        image={images[0]}
+        hours={hours}
+      />
+    </div>
+  );
 });
 
   return <div className="POPOSList">{spaces}</div>;
